7255d78a468fe31d0b3a05f57409435c32badf88
DhirenParyani <sunny.paryani@gmail.com>
2020-05-13 06:17:14 -0700

    updated login feature

--- app/src/main/java/com/android/indianspices/model/User.kt
+++ app/src/main/java/com/android/indianspices/model/User.kt
@@ -8,16 +8,18 @@ class User
     var name:String?=null
     var phone:String?=null
     var email:String?=null
+    var role:String?=null
     var password:String?=null
 
 
-    constructor(userId:String?=null,name:String,phone:String,email:String,password:String)
+    constructor(userId:String?=null,name:String,phone:String,email:String,password:String,role:String?=null)
     {
         this.userId=userId
         this.name=name
         this.phone=phone
         this.email=email
         this.password=password
+        this.role=password
 
     }
     constructor()
